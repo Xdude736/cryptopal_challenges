247340890b3f0adf9954a562032b589f302f445c
Xdude736 <38534944+Xdude736@users.noreply.github.com>
2018-09-06 15:06:05 -0400

    Update hex2base64.c
    Started the program header

--- hex2base64.c
+++ hex2base64.c
@@ -1,3 +1,13 @@
+/*
+ * Author: Xdude736
+ * Program: hex2base64
+ * Purpose: Given an input stream of hexidecimal values, with no spaces, convert it into properly formatted base64 output to the console
+ * Algorithm: 
+ *        1.) Read input as a command line argument
+ *        2.) Take the input two characters at a time and map to the appropriate hex value i.e. '4' and '2' from the console becomes 0x42
+ *        3.) Process the hex and perform bitwise operations on the raw bytes in order to get the appropriate bse64 values
+ *        4.) Print the base64 output to the console
+ */
 #include <stdio.h>
 
 int main()
